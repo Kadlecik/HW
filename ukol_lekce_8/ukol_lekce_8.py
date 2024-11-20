3ccae2a4c34e69aba3319e8ccf1409e252aa2d3f
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-11-20 21:01:48 +0100

    vytvořrní souboru 1+2

--- ukol_lekce_8/ukol_lekce_8.py
+++ ukol_lekce_8/ukol_lekce_8.py
@@ -1,1 +1,8 @@
+ Vytvoření souboru file1.txt
+with open('file1.txt', 'w') as f:
+    f.write("Ahoj svete!\nToto je prvni soubor.\nMa tri radky.\n")
+
+# Vytvoření souboru file2.txt
+with open('file2.txt', 'w') as f:
+    f.write("Ahoj svete!\nToto je druhy soubor.\nMa take tri radky.\n")
 
