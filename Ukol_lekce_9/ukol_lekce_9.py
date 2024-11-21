211aeded32427881d93e0f56a86e313432b9c359
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-11-21 18:55:30 +0100

    vytvoření jednotlivých metod pro přístup k jednotlivým polím

--- Ukol_lekce_9/ukol_lekce_9.py
+++ Ukol_lekce_9/ukol_lekce_9.py
@@ -7,3 +7,39 @@ class Car:
         self.__color = color
         self.__price = price
 
+    # Metody pro přístup k jednotlivým polím
+    def get_model(self):
+        return self.__model
+
+    def get_year(self):
+        return self.__year
+
+    def get_manufacturer(self):
+        return self.__manufacturer
+
+    def get_engine_volume(self):
+        return self.__engine_volume
+
+    def get_color(self):
+        return self.__color
+
+    def get_price(self):
+        return self.__price
+
+    def set_model(self, model):
+        self.__model = model
+
+    def set_year(self, year):
+        self.__year = year
+
+    def set_manufacturer(self, manufacturer):
+        self.__manufacturer = manufacturer
+
+    def set_engine_volume(self, engine_volume):
+        self.__engine_volume = engine_volume
+
+    def set_color(self, color):
+        self.__color = color
+
+    def set_price(self, price):
+        self.__price = price
