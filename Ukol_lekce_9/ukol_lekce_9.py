f2bc352d7e8e24add7333fc770434abbd70fb8f5
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-11-21 18:53:55 +0100

    vytvoření třídy Class Car

---
+++ Ukol_lekce_9/ukol_lekce_9.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, model, year, manufacturer, engine_volume, color, price):
+        self.__model = model
+        self.__year = year
+        self.__manufacturer = manufacturer
+        self.__engine_volume = engine_volume
+        self.__color = color
+        self.__price = price
+
