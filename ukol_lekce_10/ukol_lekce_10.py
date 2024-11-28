91e32be9f727fedcdaab8c098a3b1e26a7508316
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-11-28 19:15:51 +0100

    Vytvoření třídy coffeemachine

--- ukol_lekce_10/ukol_lekce_10.py
+++ ukol_lekce_10/ukol_lekce_10.py
@@ -9,3 +9,17 @@ class Device:
         print(f"Brand: {self.brand}")
         print(f"Model: {self.model}")
         print(f"Power: {self.power}W")
+
+class CoffeeMachine(Device):
+    def __init__(self, brand, model, power, water_capacity, coffee_type):
+        super().__init__(brand, model, power)
+        self.water_capacity = water_capacity
+        self.coffee_type = coffee_type
+
+    def make_coffee(self):
+        print(f"Making a cup of {self.coffee_type} coffee...")
+
+    def display_info(self):
+        super().display_info()
+        print(f"Water Capacity: {self.water_capacity}L")
+        print(f"Coffee Type: {self.coffee_type}")
